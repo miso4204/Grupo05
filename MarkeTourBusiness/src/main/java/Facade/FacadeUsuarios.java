214ba59aba231616f291d9a36d9736c1f988ed12
javiryma <javiryma@gmail.com>
2015-04-13 17:12:26 -0500

    Metodo proveedor

--- MarkeTourBusiness/src/main/java/Facade/FacadeUsuarios.java
+++ MarkeTourBusiness/src/main/java/Facade/FacadeUsuarios.java
@@ -3,6 +3,7 @@ package Facade;
 
 import com.marketour.persistence.*;
 import com.marketour.business.Cliente;
+import com.marketour.business.Proveedor;
 import com.marketour.domain.Compra;
 import com.marketour.persistence.Repository;
 import com.marketour.persistence.RepositoryUser;
@@ -24,7 +25,21 @@ public class FacadeUsuarios
 		Cliente bCliente=new Cliente();
 		bCliente.setDescripcion(cliente.getDescripcion());
 		bCliente.setId(cliente.getId());
+		
 		return bCliente;
+		
+	}
+	public Proveedor ConsultarProveedor(int id)
+	{
+		com.marketour.persistence.Repository<com.marketour.domain.Proveedor> repository;
+		repository = new com.marketour.persistence.Repository<com.marketour.domain.Proveedor>(com.marketour.domain.Proveedor.class);
+		com.marketour.domain.Proveedor proveedor=repository.FindById(id);
+		Proveedor bProveedor=new Proveedor();
+		bProveedor.setDescripcion(proveedor.getDescripcion());
+		bProveedor.setId(proveedor.getId());
+		bProveedor.setCuenta(proveedor.getCuenta());
+		bProveedor.setNit(proveedor.getNit());
+		return bProveedor;
 		
 	}
 
