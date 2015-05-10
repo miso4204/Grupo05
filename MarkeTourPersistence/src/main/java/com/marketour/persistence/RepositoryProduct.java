0aedaa1828e6cdd1267a2ae67666cb4a3704083c
juanda645 <juanda645@hotmail.com>
2015-05-10 01:57:40 -0500

    Implementación variabilidad anotaciones con spoon para la caracteristica de busque de productos y paquetes por ciudad. Ya esta integrado con el proyecto de derivación que hizo david.

--- MarkeTourPersistence/src/main/java/com/marketour/persistence/RepositoryProduct.java
+++ MarkeTourPersistence/src/main/java/com/marketour/persistence/RepositoryProduct.java
@@ -1,79 +1,64 @@
 package com.marketour.persistence;
 
-import java.util.List;
 
-import org.hibernate.Session;
-
-import com.marketour.domain.*;
-import com.marketour.hibernate.HibernateUtil;
-
-public class RepositoryProduct extends Repository<Producto> {
-
-	public RepositoryProduct() {
-		super(Producto.class);
-	}
-
-	public List<Producto> FindByPackage(int id) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		List<Producto> list = session
-				.createSQLQuery(
-						"SELECT prod.* FROM Producto prod INNER JOIN PaqueteProducto pp ON prod.id = pp.producto INNER JOIN Paquete pqt ON pqt.id = pp.paquete WHERE pqt.id = "
-								+ id).addEntity(Producto.class).list();
-		session.getTransaction().commit();
-		return list;
-	}
-	
-	public List<Producto> FindProducts(String fechaInicio,String fechaFin,double precio1,double precio2,int idCiudad) {
-		String rangoFechas="";
-		String innerDisponibilidad="";
-		if (!fechaInicio.equals("0") && !fechaFin.equals("0")){
-			innerDisponibilidad=" INNER JOIN Disponibilidad dp ON dp.producto=prod.id ";
-			rangoFechas=" AND dp.fechaInicio>='"+fechaInicio+"' AND dp.fechaFin<='"+fechaFin+"' ";
-		}
-		String rangoPrecios="";
-		if (precio1>0 && precio2>0){
-			rangoPrecios=" AND prod.valor>="+precio1+" and prod.valor<="+precio2;
-		}
-		String ciudad="";
-		if (idCiudad>0){
-			ciudad=" and prod.ubicacion="+idCiudad;
-		}
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		List<Producto> list = session
-				.createSQLQuery(
-						"SELECT prod.* FROM Producto prod "+innerDisponibilidad+" WHERE 1=1 "+rangoFechas+
-						rangoPrecios+ciudad).addEntity(Producto.class).list();
-		session.getTransaction().commit();
-		return list;
-	}
-	
-	
-	public List<Producto> FindProductsPorPaquete(String fechaInicio,String fechaFin,double precio1,double precio2,int idCiudad,int idPaquete) {
-		String rangoFechas="";
-		String innerDisponibilidad="";
-		if (!fechaInicio.equals("0") && !fechaFin.equals("0")){
-			innerDisponibilidad=" INNER JOIN Disponibilidad dp ON dp.producto=prod.id ";
-			rangoFechas=" AND dp.fechaInicio>='"+fechaInicio+"' AND dp.fechaFin<='"+fechaFin+"' ";
-		}
-		String rangoPrecios="";
-		if (precio1>0 && precio2>0){
-			rangoPrecios=" AND prod.valor>="+precio1+" and prod.valor<="+precio2;
-		}
-		rangoPrecios=""; //El filtro sel valor se hace en el paquete, por ahora esta variable se deja vacia con posibles modificaciones en el futuro.
-		String ciudad="";
-		if (idCiudad>0){
-			ciudad=" and prod.ubicacion="+idCiudad;
-		}
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		List<Producto> list = session
-				.createSQLQuery(
-						"SELECT prod.* FROM Producto prod "+innerDisponibilidad+" INNER JOIN Paquete_Producto pp ON prod.id = pp.producto "+
-				        "INNER JOIN Paquete pqt ON pqt.id = pp.paquete WHERE pqt.id = "+idPaquete+rangoFechas+
-						rangoPrecios+ciudad).addEntity(Producto.class).list();
-		session.getTransaction().commit();
-		return list;
-	}
+@com.marketour.persistence.Annotation(tipo = "SearchByLocation")
+public class RepositoryProduct extends com.marketour.persistence.Repository<com.marketour.domain.Producto>  {
+    public RepositoryProduct() {
+        super(com.marketour.domain.Producto.class);
+    }
+    
+    public java.util.List<com.marketour.domain.Producto>  FindByPackage(int id) {
+        org.hibernate.Session session = com.marketour.hibernate.HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        java.util.List<com.marketour.domain.Producto>  list = session.createSQLQuery(("SELECT prod.* FROM Producto prod INNER JOIN PaqueteProducto pp ON prod.id = pp.producto INNER JOIN Paquete pqt ON pqt.id = pp.paquete WHERE pqt.id = " + id)).addEntity(com.marketour.domain.Producto.class).list();
+        session.getTransaction().commit();
+        return list;
+    }
+    
+    public java.util.List<com.marketour.domain.Producto>  FindProducts(java.lang.String fechaInicio, java.lang.String fechaFin, double precio1, double precio2, int idCiudad) {
+        java.lang.String rangoFechas = "";
+        java.lang.String innerDisponibilidad = "";
+        if ((!(fechaInicio.equals("0"))) && (!(fechaFin.equals("0")))) {
+            innerDisponibilidad = " INNER JOIN Disponibilidad dp ON dp.producto=prod.id ";
+            rangoFechas = (((" AND dp.fechaInicio>='" + fechaInicio) + "' AND dp.fechaFin<='") + fechaFin) + "' ";
+        } 
+        java.lang.String rangoPrecios = "";
+        if ((precio1 > 0) && (precio2 > 0)) {
+            rangoPrecios = ((" AND prod.valor>=" + precio1) + " and prod.valor<=") + precio2;
+        } 
+        java.lang.String ciudad = "";
+        if (idCiudad > 0) {
+            ciudad = " and prod.ubicacion=" + idCiudad;
+        } 
+        org.hibernate.Session session = com.marketour.hibernate.HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        java.util.List<com.marketour.domain.Producto>  list = session.createSQLQuery(((((("SELECT prod.* FROM Producto prod " + innerDisponibilidad) + " WHERE 1=1 ") + rangoFechas) + rangoPrecios) + ciudad)).addEntity(com.marketour.domain.Producto.class).list();
+        session.getTransaction().commit();
+        return list;
+    }
+    
+    public java.util.List<com.marketour.domain.Producto>  FindProductsPorPaquete(java.lang.String fechaInicio, java.lang.String fechaFin, double precio1, double precio2, int idCiudad, int idPaquete) {
+        java.lang.String rangoFechas = "";
+        java.lang.String innerDisponibilidad = "";
+        if ((!(fechaInicio.equals("0"))) && (!(fechaFin.equals("0")))) {
+            innerDisponibilidad = " INNER JOIN Disponibilidad dp ON dp.producto=prod.id ";
+            rangoFechas = (((" AND dp.fechaInicio>='" + fechaInicio) + "' AND dp.fechaFin<='") + fechaFin) + "' ";
+        } 
+        java.lang.String rangoPrecios = "";
+        if ((precio1 > 0) && (precio2 > 0)) {
+            rangoPrecios = ((" AND prod.valor>=" + precio1) + " and prod.valor<=") + precio2;
+        } 
+        rangoPrecios = "";
+        java.lang.String ciudad = "";
+        if (idCiudad > 0) {
+            ciudad = " and prod.ubicacion=" + idCiudad;
+        } 
+        org.hibernate.Session session = com.marketour.hibernate.HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        java.util.List<com.marketour.domain.Producto>  list = session.createSQLQuery(((((((("SELECT prod.* FROM Producto prod " + innerDisponibilidad) + " INNER JOIN Paquete_Producto pp ON prod.id = pp.producto ") + "INNER JOIN Paquete pqt ON pqt.id = pp.paquete WHERE pqt.id = ") + idPaquete) + rangoFechas) + rangoPrecios) + ciudad)).addEntity(com.marketour.domain.Producto.class).list();
+        session.getTransaction().commit();
+        return list;
+    }
+    
 }
+
---
+++ MarkeTourProductDerivation/src/com/marketour/derivation/spoonannotations/Processor.java
@@ -0,0 +1,110 @@
+package com.marketour.derivation.spoonannotations;
+
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtStatement;
+import spoon.reflect.declaration.CtAnnotation;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.reference.CtReference;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.template.Substitution;
+import template.SimpleTemplate;
+
+
+public class Processor extends AbstractProcessor<CtAnnotation<?>> {
+	@Override
+	public void init() {
+		
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	@Override 
+	public void process(CtAnnotation<?> annotation) {
+		if (annotation.toString().contains("SearchByLocation")){
+			CtClass<?> target=(CtClass<?>) annotation.getParent();
+			SimpleTemplate te =new SimpleTemplate();
+			int manejaBusquedaCiudad=this.getEnvironment().getComplianceLevel();
+			String ciudadProducto="";
+			String ciudadPaquete="";
+			
+			if (manejaBusquedaCiudad==6){
+				ciudadProducto="FindProductsConCiudad";
+				ciudadPaquete="FindProductsPorPaqueteConCiudad";
+			}else{
+				ciudadProducto="FindProductsSinCiudad";
+				ciudadPaquete="FindProductsPorPaqueteSinCiudad";
+			}
+			
+			//Substitution.insertAll(annotation, te);
+			CtTypeReference<String> tString = getFactory().Type().createReference(String.class);
+			CtTypeReference<Double> tDouble = getFactory().Type().createReference(double.class);
+			CtTypeReference<Integer> tInteger = getFactory().Type().createReference(int.class);
+			for (CtMethod<?> c : target.getAllMethods()) {
+				
+				if(c.getSimpleName().toString().equals("FindProducts")){					
+					c.getBody().replace((CtStatement)			
+							Substitution.substituteMethodBody(target, te,ciudadProducto,tString,tString,tDouble,tDouble,tInteger));	
+				}
+				if(c.getSimpleName().toString().equals("FindProductsPorPaquete")){					
+					c.getBody().replace((CtStatement)			
+							Substitution.substituteMethodBody(target, te,ciudadPaquete,tString,tString,tDouble,tDouble,tInteger,tInteger));	
+				}
+				
+			}
+		}
+
+		 
+		
+		
+		
+	}	
+
+	@Override
+	public void processingDone() {
+		// TODO Auto-generated method stub
+		//jaxbWriter(fm, "./resources/model.xml", "./resources/featureide.xsd");
+		//super.processingDone();
+	}
+	
+	
+	/**
+	 * Writes the contents of a JAXB model in an xml file with identation and
+	 * blank spaces
+	 * 
+	 * @param root
+	 *            the root of the object to write
+	 * @param path
+	 *            destination of the file to create
+	 */
+	private void jaxbWriter(Object root, String path, String schema) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(root.getClass());
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, schema);
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,
+					Boolean.TRUE);
+			marshaller.marshal(root, new FileWriter(path));
+		} catch (JAXBException e) {
+			System.err
+					.println("Error while preparing to write the JAXB model in: "
+							+ path);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Error while writting the JAXB model in: "
+					+ path);
+			e.printStackTrace();
+		}
+	}
+}
