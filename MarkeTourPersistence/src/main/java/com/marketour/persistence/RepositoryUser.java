58ba0a97cfc88424716914edac97b577fc86f7c5
Andres De Castro <af.decastro879@gmail.com>
2015-05-11 22:23:20 -0500

    Login y logout. - Al hacer login y el usuario es proveedor muestra pestaña unica para el mismo.

--- MarkeTourPersistence/src/main/java/com/marketour/persistence/RepositoryUser.java
+++ MarkeTourPersistence/src/main/java/com/marketour/persistence/RepositoryUser.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.hibernate.Session;
 
+import com.marketour.domain.Proveedor;
 import com.marketour.domain.Usuario;
 import com.marketour.hibernate.HibernateUtil;
 
@@ -17,6 +18,7 @@ public class RepositoryUser extends Repository<Usuario> {
 	
 	public Map<String, String> authenticate(String login, String pass){
 		List<Usuario> usuarios = this.FindByColumn("login='"+login+"'");
+		RepositoryProvider rep = new RepositoryProvider();
 		HashMap<String, String> map = new HashMap<String, String>();
 		//El usuario no existe
 		if(usuarios.size()==0){
@@ -25,9 +27,11 @@ public class RepositoryUser extends Repository<Usuario> {
 		}else{
 			Usuario usuario = usuarios.get(0);
 			if(usuario.getPassword().equals(pass)){
+				boolean isProvider = rep.getProviderById(usuario.getId());
 				map.put("response", "true");
 				map.put("id", String.valueOf(usuario.getId()));
 				map.put("username", usuario.getNombre());
+				map.put("provider", String.valueOf(isProvider));
 				return map;
 			}
 			map.put("response", "false");
--- MarkeTourPresentation/src/main/webapp/js/login.js
+++ MarkeTourPresentation/src/main/webapp/js/login.js
@@ -2,7 +2,7 @@
  * Created by Andres on 28/04/2015.
  */
 var baseLoguedHtml =
-'<ul class="visible-lg visible-md"> ' +
+'<ul class="visible-lg visible-md" id="login-container"> ' +
     '<li style="border-left: 0px solid #eee;" class="pull-left"> ' +
         '<a href="/index.html" style="line-height: 40px; text-transform: none; color: #ffffff"> ' +
             '<i class="fa fa-user"></i> <span id="username">&nbsp;Andrés Decastro</span> ' +
@@ -11,11 +11,6 @@ var baseLoguedHtml =
     '<li style="border-left: 0px solid #eee;"> ' +
         '<a href="/payment/ShoppingCart.html" style="line-height: 40px;   text-transform: none; color: #ffffff" > ' +
             '<i class="fa fa-shopping-cart"></i> <span id="shoppingcart">&nbsp;Carrito: 0 item(s) $0.00</span> ' +
-        '</a> ' +
-    '</li> ' +
-    '<li style="border-left: 0px solid #eee;" class="pull-right"> ' +
-    	'<a href="/provider/index.html" style="line-height: 40px;   text-transform: none; color: #ffffff" > ' +
-        	'<i class="fa fa-sign-out"></i> <span id="signout">&nbsp;Proveedor</span> ' +
         '</a> ' +
     '</li> ' +
     '<li style="border-left: 0px solid #eee;" class="pull-right"> ' +
@@ -36,14 +31,25 @@ var baseNoLoguedHtml =
     '</ul> ' +
     '<a href="#" class="toggle-menu visible-sm visible-xs"> <i class="fa fa-bars"></i></a>';
 
+var baseProviderHtml =
+	'<li style="border-left: 0px solid #eee;" class="pull-right"> ' +
+		'<a href="/provider/index.html" style="line-height: 40px;   text-transform: none; color: #ffffff" > ' +
+	    	'<i class="fa fa-truck"></i> <span>&nbsp;Proveedor</span> ' +
+	    '</a> ' +
+    '</li> ';
+
  if(window.sessionStorage.getItem("usuario")){
      $("#login-div").append(baseLoguedHtml);
+     if(window.sessionStorage.getItem("provider") == "true"){
+    	 $("#login-container").append(baseProviderHtml);
+     }
      $("#username").text(sessionStorage.getItem("username"));
  }else{
      $("#login-div").append(baseNoLoguedHtml);
  }
 
 $("#signout").click(function () {
+	console.log("entra a sign out");
     sessionStorage.clear();
-    location.href = "/index.html"
+    //location.href = "/index.html"
 });
